e04b560c74872aeece4f7f8506d984628cd07a72
ajayratha4 <ajayratha4@gmail.com>
2022-08-15 23:20:34 +0530

    init

---
+++ src/main/java/com/restfulmusicvideo/restfulmusicvideo/RestfulMusicVideoApplication.java
@@ -0,0 +1,13 @@
+package com.restfulmusicvideo.restfulmusicvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulMusicVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulMusicVideoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restfulmusicvideo/restfulmusicvideo/RestfulMusicVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restfulmusicvideo.restfulmusicvideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulMusicVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
